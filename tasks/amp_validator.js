eae590a21c0c4818c68a98938a201f165673c9b4
gabriel-rausch <mail@gabriel-rausch.de>
2016-12-02 15:55:23 +0100

    remove shelljs dependency and include grunts async handler

--- tasks/amp_validator.js
+++ tasks/amp_validator.js
@@ -10,20 +10,50 @@
 
 module.exports = function(grunt) {
 
-  var shell = require('shelljs');
-  var successKey = 'PASS';
+  var fs = require('fs'),
+      amphtmlValidator = require('amphtml-validator');
 
   grunt.registerMultiTask('amp_validator', 'A Grunt plugin to wrap the validator of Google AMP.', function() {
 
     var options = this.options({
       path: ''
     });
+    var done = this.async();
 
-    var returnValidator = shell.exec('node ' + __dirname + '/../lib/ampHtmlValidator.js ' + options.path);
 
-    if (!(returnValidator.indexOf(successKey) > -1)) {
-      grunt.fail.warn('AMP Validation FAILED -- ' + returnValidator);
+    /**
+     * Read file from filesystem
+     */
+    fs.readFile(options.path, 'utf8', function(err, data) {
+      if (err) throw err;
+      validateString(data);
+    });
+
+    /**
+     * Validate string based on AMP validator
+     * @param {string} data String of AMP page
+     */
+    var validateString = function(data) {
+      amphtmlValidator.getInstance().then(function (validator) {
+        var result = validator.validateString(data);
+
+        for (var i = 0; i < result.errors.length; i++) {
+          var error = result.errors[i];
+          var msg = error.message;
+          if (error.specUrl) {
+            msg += ' (see ' + error.specUrl + ')';
+          }
+          ((error.severity === 'ERROR') ? console.error : console.warn)(msg);
+        }
+
+        if (result.status !== 'PASS') {
+          grunt.fail.warn('AMP Validation FAILED');
+        };
+
+        done();
+      });
     }
+
 
   });
 
